9525d666828432ff15ce7be72ead51a9e4690d9c
맥동우 <t6u80o@naver.com>
2026-01-02 15:57:17 +0900

    feat: auth buttons

---
+++ src/app/components/login/AuthButton.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { logout } from "@/app/login/actions";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+
+export function AuthButton({ isLoggedIn }: { isLoggedIn: boolean }) {
+  if (isLoggedIn) return <LogoutButton />;
+  return <LoginButton />;
+}
+
+function LoginButton() {
+  const router = useRouter();
+
+  return (
+    <Button onClick={() => router.push("/login")} variant="outline" className="cursor-pointer">
+      로그인
+    </Button>
+  );
+}
+
+function LogoutButton() {
+  return (
+    <Button onClick={logout} variant="outline" className="cursor-pointer">
+      로그아웃
+    </Button>
+  );
+}
